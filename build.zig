454fa67e6a8ed94d8d59965a37cb63a6939add16
ThePurplepot <fletcherw-y13@outlook.com>
2024-06-07 11:46:16 +0100

    Add RayLib Use Zig build system to include raylib and build a simple test window from zig

---
+++ build.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const opt = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "inverted-pendulum",
+        .root_source_file =  b.path("src/main.zig"),
+        .target = target,
+        .optimize = opt,
+    });
+
+    const strip = b.option(
+        bool,
+        "strip",
+        "Strip debug info to reduce binary size, defaults to false",
+    ) orelse false;
+    exe.root_module.strip = strip;
+
+    const raylib_dep = b.dependency("raylib", .{
+        .target = target,
+        .optimize = opt,
+    });
+    exe.linkLibrary(raylib_dep.artifact("raylib"));
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_cmd.step);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = opt,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const r = @cImport(@cInclude("raylib.h"));
+
+pub fn main() void {
+    r.InitWindow(960, 540, "Test Window");
+	r.SetTargetFPS(144);
+	defer r.CloseWindow();
+
+	while (!r.WindowShouldClose()) {
+		r.BeginDrawing();
+		r.ClearBackground(r.BLACK);
+		r.EndDrawing();
+	}
+}
